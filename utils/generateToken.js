f817341e0fef33ecef514c46a16f2f07dea2ac21
mhmdbrkv <barakamohamed964@gmail.com>
2024-06-26 03:42:34 +0300

    removed jwt func  async

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,13 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = async (user) => {
-  const token = await jwt.sign(
-    { userId: user._id },
-    process.env.JWT_SECRET_KEY,
-    {
-      expiresIn: process.env.JWT_EXPIRE_TIME,
-    }
-  );
+const generateToken = (user) => {
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE_TIME,
+  });
   return token;
 };
 
