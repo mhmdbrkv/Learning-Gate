4c3df9f50b50f269639a5a7c49dae4fcc712c536
mhmdbrkv <barakamohamed964@gmail.com>
2024-06-29 17:38:30 +0300

    fixes

--- utils/csrfToken.js
+++ utils/csrfToken.js
@@ -1,10 +1,3 @@
 const csrf = require("csurf");
 
-module.exports = csrf({
-  cookie: {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
-    maxAge: 900000, // 15 min
-  },
-});
+module.exports = csrf({ cookie: true });
