2b9674f262fc2a674509e87b31214a3427c31e6b
mhmdbrkv <barakamohamed964@gmail.com>
2024-07-01 16:24:36 +0300

    enhancements

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 
 // use express-rate-limit
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 5 * 60 * 1000, // 5 minutes
   limit: 100,
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
